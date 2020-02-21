8b329f6485c9163fdf6e755bf757898ea18ae676
Thoniel Solal <solalthoniel@gmail.com>
2020-02-21 09:35:17 +0100

    debut du code

---
+++ src/etats.h
@@ -0,0 +1,39 @@
+class E0 : public Etat {
+	
+};
+
+class E1 : public Etat {
+	
+};
+
+class E2 : public Etat {
+	
+};
+
+class E3 : public Etat {
+	
+};
+
+class E4 : public Etat {
+	
+};
+
+class E5 : public Etat {
+	
+};
+
+class E6 : public Etat {
+	
+};
+
+class E7 : public Etat {
+	
+};
+
+class E8 : public Etat {
+	
+};
+
+class E9 : public Etat {
+	
+};
